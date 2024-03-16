316e1183bf3ee2cc3fed9292d18e84e33d122eb1
David Ampofo <davidamp01@gmail.com>
2024-03-16 09:09:12 -0400

    organized layout

--- firstRestAPI/app.py
+++ firstRestAPI/app.py
@@ -5,9 +5,19 @@ from flask_smorest import abort
 
 app = Flask(__name__)
 
+# ----------------- GET STORE
 @app.get("/store")
 def get_stores():
     return {"stores": list(stores.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
+# ----------------- POST STORE
 
 @app.post("/store")
 def create_store():
@@ -26,6 +36,41 @@ def create_store():
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+# ----------------- DEL STORE
+
+@app.delete("/item/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message" : "Deleted store"}
+    except KeyError:
+        abort(404, message="Store not found")
+ 
+
+# ----------------- GET ALL ITEMS
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+# ----------------- GET ITEM
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found")
+
+@app.get("/item/<string:item_id>")
+def get_item_in_store(item_id):
+   try:
+       return items[item_id]
+   except KeyError:
+       abort(404, message="Item not found")
+
+# ----------------- POST ITEM
 
 @app.post("/item")
 def create_item():
@@ -59,20 +104,31 @@ def create_item():
     items[item_id] = item
     return item, 201
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
+# ----------------- DELETE ITEM
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
     try:
-        return stores[store_id]
+        del items[item_id]
+        return {"message" : "Item successfully deleted"}
     except KeyError:
-        abort(404, message="Store not found")
+        abort(404, message="Item not found")
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-@app.get("/item/<string:item_id>")
-def get_item_in_store(item_id):
-   try:
-       return items[item_id]
-   except KeyError:
-       abort(404, message="Item not found")
+# ----------------- UPDATE ITEM   
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    print("This is ITEM_DATA output:", item_data)
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload")
+    
+    try:
+        """
+        |= : dictionary merge operator
+        Details: https://blog.teclado.com/python-dictionary-merge-update-operators/ 
+        """
+        item = items[item_id]
+        item |= item_data # in place modification of item. 
+        
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
