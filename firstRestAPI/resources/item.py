c59da4fdda1e3cf9bb1170a59bea32ef036df6ce
David Ampofo <davidamp01@gmail.com>
2024-03-17 14:28:33 -0400

    typo fix in models/item.py

--- firstRestAPI/resources/item.py
+++ firstRestAPI/resources/item.py
@@ -1,62 +1,44 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
 
-from db import items
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
+
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found")
-    
+        raise NotImplementedError("Getting an item is not implemented.")
+
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message" : "Item successfully deleted"}
-        except KeyError:
-            abort(404, message="Item not found")
-    
-    @blp.arguments(ItemUpdateSchema)  
-    @blp.response(200, ItemSchema)  
+        raise NotImplementedError("Deleting an item is not implemented.")
+
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            """
-            |= : dictionary merge operator
-            Details: https://blog.teclado.com/python-dictionary-merge-update-operators/ 
-            """
-            item = items[item_id]
-            item |= item_data # in place modification of item. 
-            
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
-            
+        raise NotImplementedError("Updating an item is not implemented.")
+
+
 @blp.route("/item")
 class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values() #returns a list of vals
-    
+        raise NotImplementedError("Listing items is not implemented.")
+
     @blp.arguments(ItemSchema)
-    @blp.response(200, ItemSchema)
+    @blp.response(201, ItemSchema)
     def post(self, item_data):
-        # Want to prevent duplications
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"{item} already exist.")
-        
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id" : item_id}
-        items[item_id] = item
-        return item, 201
+        item = ItemModel(**item_data)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
+
+        return item
--- firstRestAPI/resources/store.py
+++ firstRestAPI/resources/store.py
@@ -1,48 +1,44 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
+from db import db
+from models import StoreModel
 from schemas import StoreSchema
 
-# Blueprint is used to divide the API into multiple segments
-blp = Blueprint("stores", __name__, description="Operations on stores")
 
-# Methodview: creates class whose methods routes to different endpoints 
+blp = Blueprint("Stores", __name__, description="Operations on stores")
+
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-          return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found")
-    
+        raise NotImplementedError("Getting a store is not implemented.")
+
     def delete(self, store_id):
-        try:
-             del stores[store_id]
-             return {"message" : "Deleted store"}
-        except KeyError:
-             abort(404, message="Store not found")
-             
+        raise NotImplementedError("Deleting a store is not implemented.")
+
 
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
-    
+        raise NotImplementedError("Listing stores is not implemented.")
+
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
-        #  checking if store already exist
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"'{store}' already exist.")
-        store_id = uuid.uuid4().hex # creates UUID
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
-        return store, 201
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists.",
+            )
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
 
+        return store
