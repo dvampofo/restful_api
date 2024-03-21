802550631349a54b17798811d403dfe30c182c46
David Ampofo <davidamp01@gmail.com>
2024-03-21 07:53:33 -0400

    request chaining with insomnia typo fix

--- firstRestAPI/resources/user.py
+++ firstRestAPI/resources/user.py
@@ -36,7 +36,7 @@ class UserLogin(MethodView):
         
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
             access_token = create_access_token(identity=user.id)
-            return {"access token:" : access_token}
+            return {"access_token" : access_token}
         
         abort(401, message="Invalid credentials.")
 
