4fac21f2d05e7b6464f9c20a7b6adfc1a845176a
David Ampofo <davidamp01@gmail.com>
2024-03-25 10:29:14 -0400

    new image created

--- resources/user.py
+++ resources/user.py
@@ -1,29 +1,52 @@
+import requests
+import os
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt, create_refresh_token, get_jwt_identity
+from sqlalchemy import or_
 
 from db import db
 from blocklist import BLOCKLIST
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+		auth=("api", os.getenv("MAILGUN_API_KEY")),
+		data={"from": "Excited User <mailgun@{domain}>",
+			"to": [to],
+			"subject": subject,
+			"text": body})
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409, message="A user with that username already exist.")
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+                )
+            ).first():
+            abort(409, message="A user with that username or email already exist.")
         
         user = UserModel(
             username = user_data["username"],
-            password=pbkdf2_sha256.hash(user_data["password"])
+            password=pbkdf2_sha256.hash(user_data["password"]),
+            email = user_data["email"]
         )
         db.session.add(user)
         db.session.commit()
-        
+        send_simple_message(
+            to=user.email,
+            subject="Successfully signed up",
+            body=f"Hi {user.username}, you've successfully signed up to the Stores REST API"
+        )
         return {"message" : " User created successfully"}, 201
 
 @blp.route("/login")
