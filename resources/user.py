6e41ea272137a373472295b11eb2c13d46b7e9a0
David Ampofo <davidamp01@gmail.com>
2024-03-25 10:34:03 -0400

    typo fix ub send_simple_message

--- resources/user.py
+++ resources/user.py
@@ -18,7 +18,7 @@ def send_simple_message(to, subject, body):
     return requests.post(
         f"https://api.mailgun.net/v3/{domain}/messages",
 		auth=("api", os.getenv("MAILGUN_API_KEY")),
-		data={"from": "Excited User <mailgun@{domain}>",
+		data={"from": f"Excited User <mailgun@{domain}>",
 			"to": [to],
 			"subject": subject,
 			"text": body})
